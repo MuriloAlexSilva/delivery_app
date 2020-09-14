37c824f23e25389cde53f340d8b265e071bbffdb
Murilo Silva <murilo.alex.silva@gmail.com>
2020-09-14 19:38:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# delivery_app
+ App de Delivery criado no curso do Youtube do Thizer Aplicativos
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:delivery_app/loginPage.dart';
+import 'package:flutter/cupertino.dart';
+
+main(List<String> args) {
+  runApp(LoginPage());
+}
